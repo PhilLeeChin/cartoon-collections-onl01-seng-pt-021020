92388d43c9cc8457b29882d9720207abe8c17c74
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-17 21:32:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   newarr = []
-  newarr.each_with_index do |i|
-    newstr << "#{i}"
+  array.each_with_index do |i|
+    newarr << "#{i}"
   end
 end
 
