434ba0143735830d8e84dc8f1248acd7bce3a6c2
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-17 21:27:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves# code an argument here
   # Your code here
   newarr = []
   array.each_with_index {|name, i| newarr[name] = i}
+  puts "#{i}. #{name}"
 end
 
 def summon_captain_planet# code an argument here
